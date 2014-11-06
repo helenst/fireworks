1a5fbbd586b6911656e47560dca3633f2aa445d0
Helen ST <helenst@gmail.com>
2014-11-06 21:46:46 +0000

    changed gravity, added more.

--- fireworks.py
+++ fireworks.py
@@ -4,18 +4,18 @@ import random
 pygame.init()
 
 size = width, height = 1280, 960
-gravity = 0.01
+gravity = 0.05
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
 particles = []
-for i in range(10):
+for i in range(100):
     particle = pygame.image.load("casa_silicon_ions_40.jpg")
     particlerect = particle.get_rect()
     particlerect.x = width / 2
     particlerect.bottom = height
-    speed = [random.randint(-3, 3), random.randint(-10, -5)]
+    speed = [random.uniform(-3, 3), random.uniform(-10, -5)]
     particles.append((particle, particlerect, speed))
 
 while 1:
@@ -27,10 +27,7 @@ while 1:
     for particle, particlerect, speed in particles:
         particlerect.x += speed[0]
         particlerect.y += speed[1]
-#       particlerect = particlerect.move(speed)
         speed[1] += gravity
-        print(speed)
-        print(particlerect.x, particlerect.y)
 
         screen.blit(particle, particlerect)
 
